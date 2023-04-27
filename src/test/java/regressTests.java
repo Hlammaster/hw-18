9e6c04327f28429df0085889a80ce2d9f2ff15fa
Johnny Firo <hlammaster@mail.ru>
2023-04-27 18:39:50 +0300

    added apiTests

---
+++ src/test/java/regressTests.java
@@ -0,0 +1,102 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.is;
+
+public class regressTests {
+
+    @Test
+    void getUsersListTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemes/status-scheme-responce.json"));
+
+
+    }
+
+    @Test
+    void createUserTest() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+
+
+    }
+
+    @Test
+    void updateUserAccountTest() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .patch("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+
+
+    }
+
+    @Test
+    void deleteUserTest() {
+
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().all()
+                .statusCode(204);
+
+
+
+    }
+
+    @Test
+    void unsuccessfulRegistrationTest() {
+        String body = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+
+
+    }
+
+
+}
+
