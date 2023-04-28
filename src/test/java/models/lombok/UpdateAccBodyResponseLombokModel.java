a78551e5ef64d7a67c9f104f27d69aa7503c10e8
Johnny Firo <hlammaster@mail.ru>
2023-04-28 18:28:01 +0300

    added specs

--- src/test/java/models/lombok/UpdateAccBodyResponseLombokModel.java
+++ src/test/java/models/lombok/UpdateAccBodyResponseLombokModel.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class UpdateAccBodyResponseLombokModel {
 
-    String name, updatedAt, job;
+    String name, updatedAt, job, id;
 
 
 }
--- src/test/java/tests/RegressTests.java
+++ src/test/java/tests/RegressTests.java
@@ -4,14 +4,23 @@ import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.ContentType;
 import models.lombok.UpdateAccBodyLombokModel;
 import models.lombok.UpdateAccBodyResponseLombokModel;
+import models.lombok.UsersListResponseModel;
+import models.pogo.CreateResponseModel;
 import org.junit.jupiter.api.Test;
+import specs.LoginSpecs;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.LoginSpecs.*;
+
 public class RegressTests {
+
 
     @Test
     void getUsersListTest() {
@@ -27,23 +36,24 @@ public class RegressTests {
 
 
     }
-
     @Test
     void createUserTest() {
-        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        UpdateAccBodyLombokModel accountBody = new UpdateAccBodyLombokModel();
+        accountBody.setName("morpheus");
+        accountBody.setJob("leader");
 
-        given()
-                .log().uri()
-                .body(body)
-                .contentType(ContentType.JSON)
-                .when()
-                .post("https://reqres.in/api/users")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(201)
-                .body("name", is("morpheus"))
-                .body("job", is("leader"));
+        CreateResponseModel response =
+                given(loginRequestSpec)
+                        .body(accountBody)
+                        .when()
+                        .post("/users")
+                        .then()
+                        .spec(createResponseSpec)
+                        .extract().as(CreateResponseModel.class);
+
+
+        assertThat(response.getName()).isEqualTo("morpheus");
+        assertThat(response.getJob()).isEqualTo("leader");
 
 
     }
@@ -54,21 +64,16 @@ public class RegressTests {
         accountBody.setName("morpheus");
         accountBody.setJob("zion resident");
 
-       UpdateAccBodyResponseLombokModel response = given()
-               .filter(new AllureRestAssured())
-                .log().uri()
-                .log().body()
-                .body(accountBody)
-                .contentType(ContentType.JSON)
-                .when()
-                .patch("https://reqres.in/api/users/2")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-               .extract().as(UpdateAccBodyResponseLombokModel.class);
-
-        assertThat(response.getName()).isEqualTo("morpheus");
+        UpdateAccBodyResponseLombokModel response = step("Make request", () ->
+                given(loginRequestSpec)
+                        .body(accountBody)
+                        .when()
+                        .patch("/users/2")
+                        .then()
+                        .spec(loginResponseSpec)
+                        .extract().as(UpdateAccBodyResponseLombokModel.class));
+        step("Verify response", () ->
+                assertThat(response.getName()).isEqualTo("morpheus"));
         assertThat(response.getJob()).isEqualTo("zion resident");
 
 
@@ -84,7 +89,6 @@ public class RegressTests {
                 .then()
                 .log().all()
                 .statusCode(204);
-
 
 
     }
